e50cfd9b65e0637673e1e4a49bacfc071ee21846
nicoayamen <nicolas.ayala4@gmail.com>
2024-07-06 21:33:41 -0600

    restore lost routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/about', to: 'about#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -12,9 +18,13 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
