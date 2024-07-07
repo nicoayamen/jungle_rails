cf68a16bc60ea2b6a24bd62d6e06a029862e49cd
nicoayamen <nicolas.ayala4@gmail.com>
2024-07-06 21:19:30 -0600

    add categories and count to dash

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
-class Admin::DashboardController < ApplicationController
+class Admin::DashboardController < Admin::BaseController
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
   end
 end
